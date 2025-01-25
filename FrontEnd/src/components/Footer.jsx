52dea85731d62db5859b22cf7d69d609501a5ebc
SirProg <kevinmaldonadoparedes@gmail.com>
2025-01-24 22:37:50 -0500

    ADD the components to app.jsx

---
+++ FrontEnd/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <footer className="bg-gray-800 p-4 text-white p-4">
+            <p>Footer</p>
+        </footer>
+    )
+}
---
+++ FrontEnd/src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main(){
+    return(
+        <main className="bg-gray-800 p-4 text-white p-4">
+            <p>Main</p>
+        </main>
+    )
+}
